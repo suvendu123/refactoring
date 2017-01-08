b56dd4efcde3c5c1e2d5165fbdd08c0810471840
Suvendu <suvendu.banerjee80@gmail.com>
2017-01-08 23:59:29 +0530

    Safety first, try to write some unit test. and its fail

--- trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,26 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TripServiceTest {
+	
+	private TripService service;
+	
+	@Before
+	public void setUp(){
+		service = new TripService();
+	}
+	
+	@Test
+	public void should_return_trips(){
+		List<Trip> trips = service.getTripsByUser(new User());
+		assertNotNull(trips);
+	}
 	
 }
--- trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,31 @@
 package org.craftedsw.tripservicekata.user;
 
+import static org.junit.Assert.assertEquals;
+
+import org.craftedsw.tripservicekata.trip.Trip;
+import org.junit.Before;
+import org.junit.Test;
+
 public class UserTest {
+	
+	private User user;
+	
+	@Before
+	public void setup(){
+		user = new User();
+	}
+	
+	@Test
+	public void should_add_friends(){
+		user.addFriend(new User());
+		assertEquals(1, user.getFriends().size());
+	}
+	
+	@Test
+	public void should_add_trips(){
+		user.addTrip(new Trip());
+		assertEquals(1, user.trips().size());
+	}
+	
 
 }
